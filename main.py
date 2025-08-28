dbf0d99c7bf47d214680a58f98e4e1b909d3a0a3
Zarin Tasnim <zarin_nadia@yahoo.com>
2025-08-29 00:00:36 +0600

    dark mode

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)
+
+# MySQL Config
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_DB'] = 'GottaTradeEmAll'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+# Root route
+@app.route('/')
+def home():
+    return redirect(url_for('dashboard'))
+
+# Dashboard route
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.secret_key = "your_secret_key"
+    app.run(debug=True)
